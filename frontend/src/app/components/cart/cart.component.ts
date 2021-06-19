c3dfc56680f946d43042abbd19d3b9dd2edc59d9
Rollaner <sebastianattoni@gmail.com>
2021-06-19 18:48:12 -0400

    pagina carrito funcional (falta productView)

--- frontend/src/app/components/cart/cart.component.ts
+++ frontend/src/app/components/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
+import { CartServiceService } from 'src/app/services/cart-service.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  products:Product[] = []
 
-  ngOnInit(): void {
+  constructor(private cartService:CartServiceService) { 
+
   }
 
+  ngOnInit(): void {
+    this.cartService.getCartData().subscribe(update =>{this.products=update}); //Updatea carrito con service
+  }
+
+  removeItem(item: Product){
+    console.log(item.Name);
+    this.cartService.deleteFromCart(item); //elimina item del servicio
+    delete this.products[this.products.indexOf(item)]; //elimina item del componente
+  }
+
+  purchaseCart(){
+    console.log("Cart purchased!");
+  }
 }
